49ab1ac56cec318287df6a2e54750dba7077133b
salini.s <salini.s@LTPCHE102528704>
2023-08-28 13:55:33 +0530

    Add project files.

---
+++ MiddlewaresDemo/Controllers/EmpController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiddlewaresDemo.Exceptions;
+
+namespace MiddlewaresDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            if (id ==1)
+            {
+                int a = 1;
+                int b = a / 0;
+            }
+            else if(id == 2)
+            {
+                throw new NotFoundException("Not results found");
+            }
+            else
+            {
+                throw new BadRequestException("Bad Request");
+            }
+            return Ok();
+        }
+    }
+}
---
+++ MiddlewaresDemo/middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using MiddlewaresDemo.Exceptions;
+using System.Runtime.CompilerServices;
+
+namespace MiddlewaresDemo.middlewares
+{
+    public class ExceptionMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(ILogger<ExceptionMiddleware> logger)
+        {
+            _logger = logger;       
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+               
+            }
+        }
+        private static Task HandleException(HttpContext context,Exception ex)
+        {
+            int statuscode = StatusCodes.Status500InternalServerError;
+            switch (ex)
+            {
+                case NotFoundException :
+                    statuscode = StatusCodes.Status404NotFound;
+                    break;
+                case BadRequestException :
+                    statuscode = StatusCodes.Status400BadRequest;
+                    break;
+                case DivideByZeroException:
+                    statuscode = StatusCodes.Status400BadRequest;   
+                    break;
+                default:
+                    break;
+            }
+
+            var errroresponse = new ErrorResponse
+            {
+                StatusCode = statuscode,
+                Message = ex.Message,
+            };
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statuscode;
+
+            return context.Response.WriteAsync(errroresponse.ToString());
+        }
+    }
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    //public static class ExceptionMiddlewareExtension
+    //{
+    //    public static void ConfigureExceptionMiddleware(this IApplicationBuilder app)
+    //    {
+    //        app.UseMiddleware<ExceptionMiddleware>();
+    //    } 
+    //}
+}
